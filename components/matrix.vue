3a8b3d4f2932bec34b12f5bf8dce8dc567a8126b
Andrew Heumann <andrew@hypar.io>
2020-05-02 11:39:29 -0400

    fix scrolling behavior

--- components/matrix.vue
+++ components/matrix.vue
@@ -9,15 +9,15 @@
                   :width="width"
                   :height="height"></rect>
             <student-label class="student-label"
-                           v-for="(student, k) in students"
+                           v-for="(student, k) in studentsSorted"
                            :key="`s-${k}`"
                            :x="200"
-                           :y="200 + k * 25 - scrollY"
+                           :y="200 + k * studentSpacing - scrollY * studentScrollMultiplier"
                            :student="student.fields"
                            @mouseover="hoveredStudent = student"
                            @mouseout="hoveredStudent = null"
                            :class="{fade: studentFade(student)}"></student-label>
-            <thumb v-for="(work, wk) in works"
+            <thumb v-for="(work, wk) in worksSorted"
                    :x="xPosForWork(work)"
                    :y="yPosForWork(work)"
                    :key="`w-${wk}`"
@@ -48,14 +48,19 @@ module.exports = {
         return {
             width: 1500,
             height: 1000,
+            studentSpacing: 25,
+            workSpacing: 110,
             labelColWidth: 200,
             themes: ["Space", "Collaboration", "Time", "Agency", "??"],
             scrollY: 0,
             scrollX: 0,
             maxScrollX: 100,
-            maxScrollY: 1500,
             hoveredStudent: null,
-            hoveredWork: null
+            hoveredWork: null,
+            studentsSorted: [],
+            worksSorted: [],
+            tallestColumn: 0,
+            includeStudentsWithNoWork: false
         };
     },
     props: ["works", "students"],
@@ -86,9 +91,9 @@ module.exports = {
         },
         yPosForWork(work) {
             const offset = 200;
-            const vSpacing = 110;
+            const vSpacing = this.workSpacing;
             const vIndex = this.yPositionsForWork[work.id] || 0;
-            return offset + vSpacing * vIndex - this.scrollY * 0.3;
+            return offset + vSpacing * vIndex - this.scrollY;
         },
         clamp(value, max, min) {
             return Math.min(Math.max(value, max), min);
@@ -111,6 +116,72 @@ module.exports = {
         },
         workHover(work) {
             this.hoveredWork = work;
+        },
+        processStudentsAndWorks() {
+            if (!this.students || !this.works) {
+                return;
+            }
+            let studentsToSort = [...this.students];
+            let worksToSort = [...this.works];
+            const studentsOrdered = [];
+            const worksOrdered = [];
+            const processWork = work => {
+                console.log(`processing work ${work.fields.Title}`);
+                if (work.id == "rec6JiLeophyM8M6b") {
+                    console.log(worksToSort.map(w => w.fields.Title));
+                }
+                //exit if we've run out of works
+                if (
+                    !worksToSort.length ||
+                    !work ||
+                    worksOrdered.includes(work)
+                ) {
+                    return;
+                }
+                // remove this work from the list remaining
+                worksToSort = worksToSort.filter(w => w.id != work.id);
+                if (work.id == "rec6JiLeophyM8M6b") {
+                    console.log(worksToSort.map(w => w.fields.Title));
+                }
+                // add this work to the list
+                worksOrdered.push(work);
+
+                // get the other students
+                var studentsForWork = studentsToSort.filter(s =>
+                    work.fields["Students"].includes(s.id)
+                );
+
+                // process them
+                studentsForWork.forEach(processStudent);
+            };
+            const processStudent = student => {
+                console.log(`processing student ${student.fields.Name}`);
+                //exit if we've run out of students
+                if (
+                    !studentsToSort.length ||
+                    !student ||
+                    (!student.fields.Work && !this.includeStudentsWithNoWork)
+                ) {
+                    return;
+                }
+                // remove this student from the list remaining
+                studentsToSort = studentsToSort.filter(s => s.id != student.id);
+                // add this student to the sorted list
+                studentsOrdered.push(student);
+                // get this student's works
+                const worksForStudent = worksToSort.filter(w =>
+                    w.fields["Students"].includes(student.id)
+                );
+                // process this student's works
+                worksForStudent.forEach(processWork);
+            };
+
+            while (studentsToSort.length) {
+                processStudent(studentsToSort.pop());
+            }
+
+            this.studentsSorted = studentsOrdered;
+            this.worksSorted = worksOrdered;
         }
     },
     computed: {
@@ -118,19 +189,46 @@ module.exports = {
             return `0 0 ${this.width} ${this.height}`;
         },
         yPositionsForWork() {
-            if (!this.themes || !this.works) return {};
+            if (!this.themes || !this.worksSorted) return {};
             var themeDict = {};
             var yPosDict = {};
             for (let theme of this.themes) {
                 themeDict[theme] = 0;
             }
-            this.works.forEach(work => {
+            this.worksSorted.forEach(work => {
                 var workTheme = this.themeForWork(work);
                 const themeWorkCount = themeDict[workTheme];
                 yPosDict[work.id] = themeWorkCount;
                 themeDict[workTheme] += 1;
+                if (themeDict[workTheme] > this.tallestColumn) {
+                    this.tallestColumn = themeDict[workTheme];
+                }
             });
             return yPosDict;
+        },
+        maxHeightStudents() {
+            return this.studentsSorted.length * this.studentSpacing;
+        },
+        maxHeightWorks() {
+            return this.tallestColumn * this.workSpacing;
+        },
+        maxScrollY() {
+            return this.maxHeightWorks - window.innerHeight + 400;
+        },
+        studentScrollMultiplier() {
+            var winHeight = window.innerHeight - 400;
+            return (
+                (this.maxHeightStudents - winHeight) /
+                (this.maxHeightWorks - winHeight)
+            );
+        }
+    },
+    watch: {
+        works() {
+            this.processStudentsAndWorks();
+        },
+        students() {
+            this.processStudentsAndWorks();
         }
     }
 };
